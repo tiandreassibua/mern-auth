54092fbeba27345a759c3817739d206d72e3a8f0
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-09-30 09:53:22 +0700

    initial commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,65 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+// @desc    Auth user/set token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Auth user" });
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({ name, email, password });
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
+
+// @desc    Logout user
+// @route   DELETE /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logout user" });
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "User profile" });
+});
+
+// @desc    UPDATE user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+};
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
