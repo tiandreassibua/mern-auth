d4ca01e5e48ad75c06de9cb3266c83d9ba31dece
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-09-30 17:11:02 +0700

    logout user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,6 +6,22 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
+
     res.status(200).json({ message: "Auth user" });
 });
 
@@ -24,6 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const user = await User.create({ name, email, password });
     if (user) {
+        generateToken(res, user._id);
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -39,7 +56,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   DELETE /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Logout user" });
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    
+    res.status(200).json({ message: "User logged out" });
 });
 
 // @desc    Get user profile
